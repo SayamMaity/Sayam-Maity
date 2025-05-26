ceb98d9493cd5649ef95ca0d9b34b702480694a0
Sayam Maity <sayammaity03@gmail.com>
2025-05-27 00:56:25 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Sayam-Maity/', // <-- Add this line, use your repo name
   server: {
     host: "::",
     port: 8080,
